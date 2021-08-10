eb49a272765849b784471c6ea675d0357ba5902c
LeDucLoi193 <dekaito193@gmail.com>
2021-08-10 17:29:42 +0700

    config cors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://go-out-together.vercel.app/'],
   }
 })
 require('dotenv').config()
