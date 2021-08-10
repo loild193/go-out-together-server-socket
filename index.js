f5a90e665033533cfde118748ca4801e89647fe6
LeDucLoi193 <dekaito193@gmail.com>
2021-08-10 21:25:36 +0700

    fix cors v1.3

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ io.on('connection', (socket) => {
 	})
 })
 
-const port = process.env.SERVER_PORT ?? 3001
+const port = process.env.PORT ?? 8080
 // app.listen(port, () => console.log(`Server is running at port ${port}`))
 httpServer.listen(port, () => console.log(`Port ${port}`))
