8ca5592ed0e867c6acb9c9d7d9be8f076f138862
LeDucLoi193 <dekaito193@gmail.com>
2021-08-27 15:08:41 +0700

    add bg event

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
 
-const httpServer = require("http").createServer(app)
-const io = require('socket.io')(httpServer, {
-	cors: {
-		// origin: ["http://localhost:3000", "https://go-out-together.vercel.app"]
-		origin: '*',
-		// origin: true,
-		methods: ["GET", "POST"],
-  }
-})
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    // origin: ["http://localhost:3000", "https://go-out-together.vercel.app"]
+    origin: "*",
+    // origin: true,
+    methods: ["GET", "POST"],
+  },
+});
 
-app.use(cors())
-io.on('connection', (socket) => {
-	// new user come into session
-	socket.on('new_user_coming', () => {
-		io.sockets.emit('refetch_sesion_detail')
-	})
+app.use(cors());
+io.on("connection", (socket) => {
+  // new user come into session
+  socket.on("new_user_coming", () => {
+    io.sockets.emit("refetch_sesion_detail");
+  });
 
-	// add new locations
-	socket.on('add_location', () => {
-		io.sockets.emit('refetch_add_location')
-	})
+  // add new locations
+  socket.on("add_location", () => {
+    io.sockets.emit("refetch_add_location");
+  });
 
-	// delete new locations
-	socket.on('delete_location', () => {
-		io.sockets.emit('refetch_delete_location')
-	})
+  // delete new locations
+  socket.on("delete_location", () => {
+    io.sockets.emit("refetch_delete_location");
+  });
 
-	// vote location
-	socket.on('vote', () => {
-		io.sockets.emit('refetch_vote')
-	})
-})
+  // vote location
+  socket.on("vote", () => {
+    io.sockets.emit("refetch_vote");
+  });
 
-const port = process.env.PORT ?? 8080
+  // change bgClassname
+  socket.on("new_bg_image", ({ newBgClassname }) => {
+    io.sockets.emit("change_new_bg_image", newBgClassname);
+  });
+});
+
+const port = process.env.PORT ?? 8080;
 // app.listen(port, () => console.log(`Server is running at port ${port}`))
-httpServer.listen(port, () => console.log(`Port ${port}`))
+httpServer.listen(port, () => console.log(`Port ${port}`));
